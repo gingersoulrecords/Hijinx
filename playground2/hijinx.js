c77749b320159253eb20111abf27148412d66272
Soul Plugins <gingersoulrecords@gmail.com>
2023-11-10 14:27:32 -0500

    pg2

--- playground2/hijinx.js
+++ playground2/hijinx.js
@@ -16,11 +16,49 @@
             var statements = [];
             parentElement.children().each((_, statementElement) => {
                 var statement = {
-                    type: statementElement.tagName.toLowerCase()
+                    tag: statementElement.tagName.toLowerCase(),
+                    attributes: Array.from(statementElement.attributes).map(attr => ({ name: attr.name, value: attr.value })),
+                    children: this.indexStatements($(statementElement))
                 };
+                if (statement.tag === 'select-all') {
+                    statement.targets = this.processTargets(statement, $(statementElement));
+                }
+                if (statement.children.length === 0) {
+                    delete statement.children;
+                } else {
+                    statement.children = statement.children.map(child => {
+                        child.text = $(statementElement).find(child.tag).text();
+                        return child;
+                    });
+                }
                 statements.push(statement);
             });
             return statements;
+        },
+
+        processTargets: function (statement, statementElement) {
+            var targets;
+            // Case 1: 'targets' attribute on a <select-all> tag
+            var targetsAttr = statement.attributes.find(attr => attr.name === 'targets');
+            if (targetsAttr) {
+                targets = $(targetsAttr.value);
+            }
+            // Case 2: innertext value of a <targets> child of a <select-all> tag
+            var targetsChild = statement.children.find(child => child.tag === 'targets');
+            if (targetsChild) {
+                targets = $(targetsChild.text);
+            }
+            // Case 3: jQuery product of a <targets> child's 'select-all' attribute
+            if (targetsChild && targetsChild.attributes) {
+                var selectAllAttr = targetsChild.attributes.find(attr => attr.name === 'select-all');
+                if (selectAllAttr) {
+                    targets = $(selectAllAttr.value);
+                    targetsChild.children.forEach(child => {
+                        targets = targets[child.tag](child.text);
+                    });
+                }
+            }
+            return targets;
         }
     };
 
