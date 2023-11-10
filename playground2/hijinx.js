f1c71a52804ef890f79580ed7b1eea32add4a8ca
Soul Plugins <gingersoulrecords@gmail.com>
2023-11-10 18:39:46 -0500

    pg2

--- playground2/hijinx.js
+++ playground2/hijinx.js
@@ -1,6 +1,7 @@
 (function ($) {
     var hijinx = {
         whens: [],
+        sets: [],
         indexWhens: function () {
             var whenElements = $('when');
             whenElements.each((_, whenElement) => {
@@ -35,18 +36,42 @@
             });
             return statements;
         },
-
+        processSets: function () {
+            this.whens.forEach(when => {
+                when.statements.forEach(statement => {
+                    if (statement.tag === 'set') {
+                        var setName = statement.attributes.find(attr => attr.name === 'name').value;
+                        var setValue = this.processStatements($(statement.children));
+                        this.sets.push({ name: setName, value: setValue });
+                    }
+                });
+            });
+        },
+        processStatements: function (statements) {
+            // Process the statements and return the result
+            // This function needs to be implemented
+        },
         processTargets: function (statement, statementElement) {
             var targets;
             // Case 1: 'targets' attribute on a <select-all> tag
             var targetsAttr = statement.attributes.find(attr => attr.name === 'targets');
             if (targetsAttr) {
                 targets = $(targetsAttr.value);
+                if (targets.length === 0) {
+                    console.log(`Warning: No elements found for selector "${targetsAttr.value}" in <select-all> tag's "targets" attribute.`);
+                } else {
+                    console.log('case 1: targets =', targets);
+                }
             }
             // Case 2: innertext value of a <targets> child of a <select-all> tag
             var targetsChild = statement.children.find(child => child.tag === 'targets');
-            if (targetsChild) {
-                targets = $(targetsChild.text);
+            if (targetsChild && (!targetsChild.children || !targetsChild.children.length)) {
+                targets = $(statementElement.find('targets').text());
+                if (targets.length === 0) {
+                    console.log(`Warning: No elements found for selector "${targetsChild.text}" in <targets> child tag.`);
+                } else {
+                    console.log('case 2: targets =', targets);
+                }
             }
             // Case 3: jQuery product of a <targets> child's 'select-all' attribute
             if (targetsChild && targetsChild.attributes) {
@@ -56,15 +81,23 @@
                     targetsChild.children.forEach(child => {
                         targets = targets[child.tag](child.text);
                     });
+                    if (targets.length === 0) {
+                        console.log(`Warning: No elements found for selector "${selectAllAttr.value}" in <targets> child tag's "select-all" attribute.`);
+                    } else {
+                        console.log('case 3: targets =', targets);
+                    }
                 }
             }
             return targets;
         }
     };
 
+    window.hijinx = hijinx;
+
     // Call the indexWhens method when the window has loaded
     $(window).on('load', function () {
         hijinx.indexWhens();
+        hijinx.processSets();
         console.log('hijinx:', hijinx);
     });
 })(jQuery);
