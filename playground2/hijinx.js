a2e298baa86c5e5e729b4db45ac367be02db6f84
Soul Plugins <gingersoulrecords@gmail.com>
2023-11-14 19:54:17 -0500

    if statements!

--- playground2/hijinx.js
+++ playground2/hijinx.js
@@ -64,19 +64,20 @@
         },
         init: function () {
             this.tagHandlers = {
-                'css': this.processCssStatement.bind(this),
-                'attr': this.processAttrStatement.bind(this),
                 'add-class': this.processAddClassStatement.bind(this),
-                'on-click': this.processOnClickStatement.bind(this),
+                'attr': this.processAttrStatement.bind(this),
+                'css': this.processCssStatement.bind(this),
+                'hide': this.processHideStatement.bind(this),
                 'insert-after': this.processInsertAfterStatement.bind(this),
                 'match-height': this.processMatchHeightStatement.bind(this),
+                'on-click': this.processOnClickStatement.bind(this),
                 'show': this.processShowStatement.bind(this),
-                'hide': this.processHideStatement.bind(this),
+                'store': this.processStoreStatement.bind(this),
+                'if': this.processIfStatement.bind(this),
 
                 // ... add handlers for other tags here ...
             };
         },
-
         processStatements: function (statements, whenIndex) {
             statements.forEach(statement => {
                 if (statement.tag === 'select-all') {
@@ -89,6 +90,20 @@
                             handler(child, targets, whenIndex);
                         }
                     });
+                } else if (statement.tag === 'store') {
+                    // Get the handler for the 'store' tag
+                    var handler = this.tagHandlers[statement.tag];
+                    if (handler) {
+                        // Call the handler with the necessary arguments
+                        handler(statement, [], whenIndex);
+                    }
+                } else if (statement.tag === 'if') {
+                    // Get the handler for the 'if' tag
+                    var handler = this.tagHandlers[statement.tag];
+                    if (handler) {
+                        // Call the handler with the necessary arguments
+                        handler(statement, [], whenIndex);
+                    }
                 }
                 // ... handle other statement types ...
             });
@@ -119,6 +134,36 @@
                 $(target).css(cssProperties);
             });
         },
+        processHideStatement: function (statement, targets) {
+            targets.forEach(target => {
+                $(target).hide();
+            });
+        },
+        processIfStatement: function (statement, targets, whenIndex) {
+            // Get the value to compare it to
+            var value = statement.attributes.find(attr => attr.name === 'value').value;
+            console.log('Value to compare:', value);
+            // Check if the condition is true
+            var condition = false;
+            var storeAttr = statement.attributes.find(attr => attr.name === 'store');
+            var setAttr = statement.attributes.find(attr => attr.name === 'set');
+            if (storeAttr) {
+                console.log('Checking store:', storeAttr.value);
+                condition = store.get(storeAttr.value) === value;
+                console.log('Store condition:', condition);
+            } else if (setAttr) {
+                console.log('Checking set:', setAttr.value);
+                condition = this.sets[setAttr.value] === value;
+                console.log('Set condition:', condition);
+            }
+            if (condition) {
+                console.log('Condition is true, processing children...');
+                // If the condition is true, process the children of the 'if' tag
+                this.processStatements(statement.children, whenIndex);
+            } else {
+                console.log('Condition is false, not processing children.');
+            }
+        },
         processInsertAfterStatement: function (statement, targets) {
             var insertAfterTargets = statement.children.find(child => child.tag === 'select-all').targets;
             targets.forEach(target => {
@@ -147,12 +192,14 @@
                 $(target).show();
             });
         },
-
-        processHideStatement: function (statement, targets) {
-            targets.forEach(target => {
-                $(target).hide();
+        processStoreStatement: function (statement, targets) {
+            statement.children.forEach(child => {
+                var key = child.tag;
+                var value = child.text;
+                store.set(key, value);
             });
         },
+
         processTargets: function (statement, statementElement) {
             var targets = [];
             // Case 1: 'targets' attribute on a <select-all> tag
