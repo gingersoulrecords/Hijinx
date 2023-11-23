8f41d6423b1773404565a02a8ab122602c2bdc75
Soul Plugins <gingersoulrecords@gmail.com>
2023-11-23 17:14:02 -0500

    adding hover pseudo

--- preprocessing/process.js
+++ preprocessing/process.js
@@ -5,39 +5,104 @@ function escapeClassName(className) {
         .replace(/\]/g, "\\]");
 }
 
+// function processHTML(htmlString) {
+//     let styleTagContent = "";
+//     const parser = new DOMParser();
+//     const doc = parser.parseFromString(htmlString, "text/html");
+
+//     const sizeModifiers = {
+//         "md:": "768px",
+//         "lg:": "1024px",
+//         "xl:": "1366px"
+//     };
+
+//     // Iterate through the elements of the isolated DOM object
+//     doc.body.querySelectorAll("*").forEach((elem) => {
+//         const classList = elem.className.split(/\s+/);
+//         classList.forEach((className) => {
+//             const match = className.match(/([a-z0-9\-:]+)-\[(.+)\]/i); // Changed regex here
+//             if (match) {
+//                 const prefix = match[1] || "";
+//                 let value = match[2].replace(/_/g, " ");
+//                 const escapedClassName = escapeClassName(className);
+//                 let ruleAdded = false;
+
+//                 if (value.startsWith("--")) {
+//                     value = `var(${value})`;
+//                 }
+
+//                 for (const [modifier, minWidth] of Object.entries(sizeModifiers)) {
+//                     if (prefix.startsWith(modifier)) {
+//                         styleTagContent += `@media (min-width: ${minWidth}) {
+//                             .${escapedClassName} {
+//                                 ${prefix.slice(modifier.length)}: ${value};
+//                             }
+//                         }\n`;
+//                         ruleAdded = true;
+//                         break;
+//                     }
+//                 }
+
+//                 if (!ruleAdded) {
+//                     styleTagContent += `.${escapedClassName} {
+//                         ${prefix}: ${value};
+//                     }\n`;
+//                 }
+//             }
+//         });
+//     });
+
+//     return `<style>\n${css_beautify(styleTagContent)}\n</style>\n\n${htmlString}`;
+// }
+
 function processHTML(htmlString) {
     let styleTagContent = "";
     const parser = new DOMParser();
     const doc = parser.parseFromString(htmlString, "text/html");
 
+    const sizeModifiers = {
+        "md:": "768px",
+        "lg:": "1024px",
+        "xl:": "1366px"
+    };
+
     // Iterate through the elements of the isolated DOM object
     doc.body.querySelectorAll("*").forEach((elem) => {
         const classList = elem.className.split(/\s+/);
         classList.forEach((className) => {
-            const match = className.match(/([a-z0-9\-:]+)?-\[(.+)\]/i);
+            const match = className.match(/([a-z0-9\-:]+)-\[(.+)\]/i);
             if (match) {
                 const prefix = match[1] || "";
-                const value = match[2].replace(/_/g, " ");
+                let value = match[2].replace(/_/g, " ");
                 const escapedClassName = escapeClassName(className);
-                if (prefix.startsWith("lg:")) {
-                    styleTagContent += `@media (min-width: 1024px) {
-                        .${escapedClassName} {
-                            ${prefix.slice(3)}: ${value};
+                let ruleAdded = false;
+
+                if (value.startsWith("--")) {
+                    value = `var(${value})`;
+                }
+
+                if (prefix.startsWith("hover:")) {
+                    styleTagContent += `@media (hover: hover) and (pointer: fine) {
+                        .${escapedClassName}:hover {
+                            ${prefix.slice(6)}: ${value};
                         }
                     }\n`;
-                } else if (prefix.startsWith("md:")) {
-                    styleTagContent += `@media (min-width: 768px) {
-                        .${escapedClassName} {
-                            ${prefix.slice(3)}: ${value};
-                        }
-                    }\n`;
-                } else if (prefix.startsWith("xl:")) {
-                    styleTagContent += `@media (min-width: 1366px) {
-                        .${escapedClassName} {
-                            ${prefix.slice(3)}: ${value};
-                        }
-                    }\n`;
-                } else {
+                    ruleAdded = true;
+                }
+
+                for (const [modifier, minWidth] of Object.entries(sizeModifiers)) {
+                    if (!ruleAdded && prefix.startsWith(modifier)) {
+                        styleTagContent += `@media (min-width: ${minWidth}) {
+                            .${escapedClassName} {
+                                ${prefix.slice(modifier.length)}: ${value};
+                            }
+                        }\n`;
+                        ruleAdded = true;
+                        break;
+                    }
+                }
+
+                if (!ruleAdded) {
                     styleTagContent += `.${escapedClassName} {
                         ${prefix}: ${value};
                     }\n`;
